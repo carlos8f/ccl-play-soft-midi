fc3715722ca0d4cdc1b595e3793b2d165a6c6d0f
Carlos Rodriguez <carlos@s8f.org>
2012-08-02 11:57:46 -0700

    try to implement program change and play note

--- play.lisp
+++ play.lisp
@@ -1,6 +1,12 @@
 (defVar *graph* nil "the AudioUnit graph")
 (defVar *synth-unit* nil "the synth unit")
+
 (defVar *channel* 0 "MIDI channel")
+(defVar *control-change* #xB "control change")
+(defVar *program-change* #xC "program change")
+(defVar *bank-msb-control* 0 "bank MSB control")
+(defVar *bank-lsb-control* 32 "bank LSB control")
+(defVar *note-on* #x9 "note on")
 
 (objc:load-framework "AudioUnit" :audiounit)
 (objc:load-framework "AudioToolbox" :audiotoolbox)
@@ -46,4 +52,46 @@
       (#_AUGraphConnectNodeInput *graph* (%ptr-to-int Limiter-Node) 0 (%ptr-to-int Output-Node) 0)
       (#_AUGraphNodeInfo *graph* (%ptr-to-int Synth-Node) Synth-Description Synth-Unit)
       (when (%null-ptr-p Synth-Unit) (error "could not get NodeInfo for synth"))
-      (setq *synth-unit* Synth-Unit))))
+      (setq *synth-unit* Synth-Unit)
+      (#_AUGraphInitialize *graph*)
+      )))
+
+(defun dispose-graph()
+  "clean up the graph object"
+  (#_AUGraphStop *graph*)
+  (#_DisposeAUGraph *graph*))
+
+(defun play()
+  "play a scale"
+  ; create the graph and synth
+  (create-synth)
+  ; set the bank
+  (#_MusicDeviceMIDIEvent *synth-unit*
+                          (logior (ash *control-change* 4) *channel*)
+                          *bank-msb-control*
+                          0
+                          0)
+
+  (#_MusicDeviceMIDIEvent *synth-unit*
+                          (logior (ash *program-change* 4) *channel*)
+                          0 ; program change number
+                          0 ; ?
+                          0) ; sample offset
+
+  
+  (#_AUGraphStart *graph*)
+
+  (#_MusicDeviceMIDIEvent *synth-unit*
+                          (logior (ash *note-on* 4) *channel*)
+                          60 ; note number
+                          127 ; on velocity
+                          0) ; ?
+  (sleep 5)
+
+  (#_MusicDeviceMIDIEvent *synth-unit*
+                          (logior (ash *note-on* 4) *channel*)
+                          60 ; note number
+                          0 ; off velocity
+                          0) ; ?
+
+  (dispose-graph))
